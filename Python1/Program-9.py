2ca014ae5d128fe7ffdff1c634e265f13cf36fd0
vanshikaagarwal130202 <69415611+vanshikaagarwal130202@users.noreply.github.com>
2020-09-02 16:10:20 +0530

    Create Program-9.py

---
+++ Python1/Program-9.py
@@ -0,0 +1,25 @@
+def printFibonacciNumbers(n): 
+
+      
+
+    f1 = 0
+
+    f2 = 1
+
+    if (n < 1): 
+
+        return
+
+    for x in range(0, n): 
+
+        print(f2, end = " ") 
+
+        next = f1 + f2 
+
+        f1 = f2 
+
+        f2 = next
+
+        
+
+printFibonacciNumbers(7) 
